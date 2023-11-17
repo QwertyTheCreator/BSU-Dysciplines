aba6318869b9df4fbbb22a198e6fbba137f7f357
Alexandr <alexandrglazko@mail.ru>
2023-11-17 20:13:36 +0300

    Fixed

--- Operating-Systems/4. ProcessesSyncronization/Reciever/Receiver.cpp
+++ Operating-Systems/4. ProcessesSyncronization/Reciever/Receiver.cpp
@@ -1,103 +1,107 @@
-#include <fstream>
+п»ї#include <fstream>
 #include <iostream>
-#include <Windows.h>
+#include <windows.h>
 #include <conio.h>
 #include <string>
-#include "HelpFunctions.h"
+#include<cstdio>
 
 
 int main()
 {
-	std::string fileName;
-	std::cout << "Enter binary file name: ";
-	std::getline(std::cin, fileName);
+	std::string bin_file_name;
+	std::cout << "Enter binary file name\n";
+	std::cin >> bin_file_name;
 
-	CreateBinaryFile(fileName);
+	long long number_of_notes;
+	std::cout << "Enter number of notes:\n";
+	std::cin >> number_of_notes;
 
-	int numberOfNotes;
-	EnterNums(numberOfNotes, "Enter number of notes in message: ");
+	std::fstream file;
+	file.open(bin_file_name, std::ios::trunc);
+	file.open(bin_file_name, std::ios::out);
+	file.close();
 
-	int numberOfSenders;
-	EnterNums(numberOfSenders, "Enter number of senders processes: ");
+	int number_of_senders;
+	std::cout << "Enter number of Sender processes:\n";
+	std::cin >> number_of_senders;
 
-	HANDLE hInputReadySemaphore = CreateSemaphore(NULL, 0, numberOfNotes, L"Input Semaphore started");
-	if (hInputReadySemaphore == NULL)
-	{
+	HANDLE hWriteReadySemaphore = CreateSemaphore(NULL, 0, number_of_notes, L"Input_semaphore_started");
+	if (hWriteReadySemaphore == NULL)
 		return GetLastError();
-	}
-
-	HANDLE hOutputReadySemaphore = CreateSemaphore(NULL, 0, numberOfNotes, L"Output Semaphore started");
-	if (hOutputReadySemaphore == NULL)
-	{
-		return GetLastError();
-	}
 
 	HANDLE hMutex = CreateMutex(NULL, 0, L"mutex");
 
-	HANDLE* hEventStarted = new HANDLE[numberOfSenders];
+	HANDLE* hEventStarted = new HANDLE[number_of_senders];
+	HANDLE hOutput = CreateEvent(NULL, 0, 0, L"read");
+
+	LPWSTR lpwstrSenderProcessCommandLine;
 	STARTUPINFO si;
 	PROCESS_INFORMATION pi;
-	LPWSTR lpwstrSenderCmd;
-	for (int i = 0; i < numberOfSenders; i++)
+
+	for (int i = 0; i < number_of_senders; ++i)
 	{
-		std::string senderCmd = "Sender.exe " + fileName;
-		std::wstring convertToLpwstr = std::wstring(senderCmd.begin(), senderCmd.end());
-		lpwstrSenderCmd = &convertToLpwstr[0];
+		std::string sender_cmd = "Sender.exe " + bin_file_name;
+		std::wstring converting_sender_to_lpwstr = std::wstring(sender_cmd.begin(), sender_cmd.end());
+		lpwstrSenderProcessCommandLine = &converting_sender_to_lpwstr[0];
+
 		ZeroMemory(&si, sizeof(STARTUPINFO));
 		si.cb = sizeof(STARTUPINFO);
-		if (!CreateProcess(NULL, lpwstrSenderCmd, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
+
+		if (!CreateProcess(NULL, lpwstrSenderProcessCommandLine, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi))
 		{
-			system("cls");
-			std::cout << "The Sender Process is not started.\n";
+			std::cout << "The Sender process is not started.\n";
 			return GetLastError();
 		}
 
-		hEventStarted[i] = CreateEvent(NULL, FALSE, FALSE, L"Process Started");
+		hEventStarted[i] = CreateEvent(NULL, FALSE, FALSE, L"Process_started");
 
 		if (hEventStarted[i] == NULL)
-		{
 			return GetLastError();
-		}
+
 		CloseHandle(pi.hProcess);
-		CloseHandle(pi.hThread);
 	}
 
-	WaitForMultipleObjects(numberOfSenders, hEventStarted, TRUE, INFINITE);
+	WaitForMultipleObjects(number_of_senders, hEventStarted, TRUE, INFINITE);
 
-	ReleaseMutex(hMutex);
+	std::string number;
 
-	int key;
-	EnterOption(key);
+	file.open(bin_file_name, std::ios::in);
 
-	std::fstream file(fileName, std::ios::in);
 	while (true)
 	{
-		if (key == 1)
+		std::cout << "\nEnter 1 to read message\nEnter 0 to exit process\n";
+		std::cin >> number;
+
+		if (number == "1")
 		{
 			std::string message;
-			WaitForSingleObject(hInputReadySemaphore, INFINITE);
-			WaitForSingleObject(hMutex, INFINITE);
-			std::getline(file, message);
-			std::cout << message;
-			ReleaseSemaphore(hOutputReadySemaphore, 1, NULL);
-			ReleaseMutex(hMutex);
 
-			EnterOption(key);
+			WaitForSingleObject(hWriteReadySemaphore, INFINITE);
+			WaitForSingleObject(hMutex, INFINITE);
+
+			std::getline(file, message);
+
+			std::cout << message;
+
+			SetEvent(hOutput);
+			ReleaseMutex(hMutex);
 		}
-		else
+		else if (number == "0")
 		{
 			std::cout << "Process ended";
 			break;
 		}
+		else if (number != "0" && number != "1")
+			std::cout << "\nIncorrect value. Enter again\n";
 	}
 
-	CloseHandle(hInputReadySemaphore);
-	CloseHandle(hOutputReadySemaphore);
-	for (int i = 0; i < numberOfSenders; i++)
-	{
+	file.close();
+
+	CloseHandle(hWriteReadySemaphore);
+	CloseHandle(hOutput);
+
+	for (int i = 0; i < number_of_senders; i++)
 		CloseHandle(hEventStarted[i]);
-	}
-	delete[] hEventStarted; // Будет ли ошибка
 
 	return 0;
 }
--- Operating-Systems/4. ProcessesSyncronization/Sender/Sender.cpp
+++ Operating-Systems/4. ProcessesSyncronization/Sender/Sender.cpp
@@ -1,83 +1,82 @@
-#include <fstream>
+﻿#include <fstream>
 #include <windows.h>
 #include <conio.h>
 #include <iostream>
-#include "HelpFunctions.h"
+#include<string>
 
-int main(int argc, char* argv[]) {
-	std::string fileName = argv[1];
+
+int main(int argc, char* argv[])
+{
+	std::string bin_file_name = argv[1];
 	std::fstream file;
 
-	HANDLE hStartEvent = OpenEvent(EVENT_MODIFY_STATE, FALSE, L"Process Started");
+	HANDLE hStartEvent = OpenEvent(EVENT_MODIFY_STATE, FALSE, L"Process_started");
 	if (hStartEvent == NULL)
-	{
-		std::cout << "Open event failed. \nInput any char to exit.\n";
-		std::cin.get();
-		return GetLastError();
-	}
-
-	HANDLE hInputReadySemaphore = OpenSemaphore(EVENT_ALL_ACCESS, FALSE, L"Input Semaphore started");
-	if (hInputReadySemaphore == NULL)
 		return GetLastError();
 
-	HANDLE hOutputReadySemaphore = OpenSemaphore(EVENT_ALL_ACCESS, FALSE, L"Output Semaphore started");
-	if (hOutputReadySemaphore == NULL)
+	HANDLE hWriteReadySemaphore = OpenSemaphore(EVENT_ALL_ACCESS, FALSE, L"Input_semaphore_started");
+	if (hWriteReadySemaphore == NULL)
 		return GetLastError();
 
-	HANDLE hMutex = OpenMutex(SYNCHRONIZE, FALSE, L"mut ex");
+	HANDLE hMutex = OpenMutex(SYNCHRONIZE, FALSE, L"mutex");
+
+	HANDLE hOutput = OpenEvent(EVENT_ALL_ACCESS, 0, L"read");
 
 	SetEvent(hStartEvent);
 
-	int key;
-	EnterOption(key);
+	std::cout << "Event was started\n";
 
-	while (true) {
-		if (key == 1) {
+	std::string number;
 
+	while (true)
+	{
+		std::cout << "Enter 1 to write message\nEnter 0 to exit process\n";
+		std::cin >> number;
+
+		if (number == "1")
+		{
 			WaitForSingleObject(hMutex, INFINITE);
-			file.open(fileName, std::ios::out | std::ios::app);
+
 			std::string msg;
-			std::cout << "Input message to add\n";
+			std::cout << "Enter message to add\n";
 			std::cin >> msg;
-			char message[20];
+
+			char message[21];
+
 			for (int i = 0; i < msg.length(); i++)
-			{
 				message[i] = msg[i];
-			}
+
 			for (int i = msg.length(); i < 20; i++)
-			{
 				message[i] = '\0';
-			}
 
-			message[19] = '\n';
+			message[20] = '\n';
+
 			ReleaseMutex(hMutex);
-			ReleaseSemaphore(hOutputReadySemaphore, 1, NULL);
-			if (ReleaseSemaphore(hInputReadySemaphore, 1, NULL) != 1) {
-				std::cout << "file is full";
-				ReleaseSemaphore(hOutputReadySemaphore, 1, NULL);
-				WaitForSingleObject(hOutputReadySemaphore, INFINITE);
-				ReleaseSemaphore(hOutputReadySemaphore, 1, NULL);
-				ReleaseSemaphore(hInputReadySemaphore, 1, NULL);
-				for (int i = 0; i < 20; i++)
-				{
-					file << message[i];
-				}
 
+			if (ReleaseSemaphore(hWriteReadySemaphore, 1, NULL) != 1)
+			{
+				std::cout << "File is full\n";
+
+				ResetEvent(hOutput);
+				WaitForSingleObject(hOutput, INFINITE);
 			}
-			else {
-				for (int i = 0; i < 20; i++)
-				{
+			else
+			{
+				file.open(bin_file_name, std::ios::out | std::ios::app);
+
+				for (int i = 0; i < 21; i++)
 					file << message[i];
-				}
+
+				file.close();
 			}
-			file.close();
-			EnterOption(key);;
 		}
-
-		if (key == 2) {
-			std::cout << "Process ended.";
+		else if (number == "0")
+		{
+			std::cout << "Process ended";
 			break;
 		}
+		else if (number != "0" && number != "1")
+			std::cout << "\nIncorrect value. Enter again\n";
 	}
 
 	return 0;
